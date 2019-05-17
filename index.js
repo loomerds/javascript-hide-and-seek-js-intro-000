207b3684b1b6a6adf7c36d5809b08ccccbcea840
Doug Loomer <loomerds@gmail.com>
2019-05-17 19:22:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let deepest = document.querySelector('#grand-node');
+  while(deepest.firstElementChild!==null) {
+    deepest = deepest.firstElementChild;
+  }
+  return deepest;
+}
+
+function increaseRankBy(n) {
+  let targetNodes = document.querySelectorAll('.ranked-list li');
+  for(let i=0; i<targetNodes.length; i++) {
+    targetNodes[i].innerHTML = parseInt(targetNodes[i].innerHTML)+n;
+  }
+}
