145155bc0a7d9728cd46a9ed3529ba0055004ec0
Doug Loomer <loomerds@gmail.com>
2019-05-18 12:14:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ function nestedTarget() {
 function deepestChild() {
   let deepest = document.querySelector('#grand-node');
   while(deepest.firstElementChild!==null) {
+<<<<<<< HEAD
     deepest = deepest.firstElementChild;
   }
   return deepest;
+=======
+    //if(deepest.firstElementChild!==null) {
+      deepest = deepest.firstElementChild;
+    //} else {
+    }
+      return deepest;
+    //}
+  }
+  //console.log(deepest);
+//  return deepest[deepest.length-1];
+
+//  return targetNodes[targetNodes.length-1];
+>>>>>>> 5476dbadfa361adb4ce5a44cc111bbdb447d2d13
 }
 
 function increaseRankBy(n) {
